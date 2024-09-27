1a65988bbb74d3819ded1db787feafefff9c50e1
oae26 <oae26@njit.edu>
2024-09-27 09:12:19 -0400

    Homework 3 initial Calculations

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,9 +1,20 @@
 """Provides an arithmetic addition function"""
+class Calculation:
+    def __init__(self,a,b,operation):
+        self.a = a
+        self.b = b
+        self.operation = operation # Store the operation function here
 
-def add(a,b):
-    """adds two numbers"""
-    return a + b
-def subtract(a,b):
+        def get_result(self):
+            # Call the stored operation with a and b
+class Calculator:
+    @staticmethod
+    def add(a,b):
+        calculation = Calculation(a,b,add) # Pass the add function from calculator
+        return calculation.get_result()
+    @staticmethod
+    def subtract(a,b):
+        calculation = Calculation(a,b,subtract) #Pass the subtract function
     """subtracts two numbers"""
     return a - b
 def divide(a,b):
